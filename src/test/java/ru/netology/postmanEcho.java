bc9244c4327c02781a31814aa14e2cdd719e13a6
anay333 <anay_1991@bk.ru>
2022-02-05 22:58:38 +0500

    добавлены файлы

---
+++ src/test/java/ru/netology/postmanEcho.java
@@ -0,0 +1,30 @@
+package ru.netology;
+
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.Argument;
+import io.restassured.specification.RequestSpecification;
+import org.hamcrest.Matcher;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalToObject;
+import static org.hamcrest.Matchers.is;
+
+
+public class postmanEcho {
+    @Test
+    void shouldReturnData() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data",is("some data"))
+        ;
+    }
+}
